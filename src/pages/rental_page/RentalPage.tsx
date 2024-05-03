1d7d486674ab27fea17cdbc5f03bd7a3725b8381
nm090909 <nm09090@naver.com>
2024-05-03 22:10:33 +0900

    [get flavor list]

--- src/pages/rental_page/RentalPage.tsx
+++ src/pages/rental_page/RentalPage.tsx
@@ -1,5 +1,5 @@
 import PageHeader from "../../components/header/PageHeader";
-import {useContext, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {SidebarContext} from "../../context/SidebarContext";
 import {
     DatePickProps,
@@ -7,12 +7,13 @@ import {
     SelectTableItem,
     SelectTableProps,
     SERVER_URL,
-    sidebarPanel
+    sidebarPanel, TableData
 } from "../../config/Config";
 import SubHead from "../../components/subhead/SubHead";
 import InputBox from "../../components/InputBox/InputBox";
 import DatePick from "../../components/datePick/DatePick";
 import SelectTable from "../../components/selectTable/SelectTable";
+import Loading from "../../components/loading/Loading";
 
 
 export default function RentalPage() {
@@ -45,16 +46,38 @@ export default function RentalPage() {
 
     const [flavor, setFlavor] = useState<number>(0);
     const flavorChange = (value: number) => {setFlavor(value)}
-    //TODO flavor_list라는 경로로 받아올 수 있게 수정하였음
+
+    const flavorUrl = SERVER_URL + "/openstack/flavor_list";
+    const [flavorLoading, setFlavorLoading] = useState<boolean>(true);
+    const [flavorLoadError, setFlavorLoadError] = useState<boolean>(false);
+    const [flavorData, setFlavorData] = useState<SelectTableItem[]>([])
+    const makeTableData = (data:any) => {
+        setFlavorData([]);
+        data.map((item:any) => {
+            setFlavorData((prev:SelectTableItem[]) => [...prev, {
+                name: item.name,
+                cpu: item.cpu,
+                ram: item.ram,
+                disk: item.disk
+            }]);
+        });
+    };
+    useEffect(() => {
+        fetch(flavorUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json()).then((result) => {
+            makeTableData(result);
+            setFlavorLoading(false);
+        }).catch((error) => {
+            setFlavorLoadError(true);
+        });
+    }, []);
     //TODO 추가적으로, image_list라는 API 추가하여 이미지 이름들 받아올 수 있게 하였음
     //TODO 비밀번호를 설정할 것인지, 키페어를 이용할 것인지를 사용자가 선택할 수 있어야 함
-    const testData: SelectTableItem[] = [
-        {name:"m1.nano", cpu:1, ram:128, disk:1},
-        {name:"a123e", cpu:2, ram:3, disk:4},
-        {name:"aeee", cpu:1165, ram:12, disk:1222},
-        {name:"aaae", cpu:154, ram:51, disk:11}
-    ]
-    const selectTableProps: SelectTableProps = {rows:testData, change: flavorChange}
+    const selectTableProps: SelectTableProps = {rows:flavorData, change: flavorChange}
     const url:string = SERVER_URL + "/openstack/rental"
     //TODO 요청결과에 따른 반응 수정 필요
     //testData[flavor].name
@@ -67,7 +90,7 @@ export default function RentalPage() {
                 start_date   : startDate.toISOString().split("T")[0],
                 end_date     : endDate.toISOString().split("T")[0],
                 image_name   : "cirros-0.6.2-x86_64-disk",
-                flavor_name  : "m1.nano",
+                flavor_name  : flavorData[flavor].name,
                 network_name : "shared",
                 password     : password,
                 cloud_init   : ""
@@ -100,6 +123,8 @@ export default function RentalPage() {
             <h5>cirros-0.6.2-x86_64-disk</h5>
             {SubHead("Flavor")}
             {SelectTable(selectTableProps)}
+            {flavorLoadError ? SubHead("서버로부터 응답이 없습니다.") :
+                flavorLoading ? <Loading/> : null}
             <button className="submit_button" onClick={(e) => {submit(e)}}>대여 신청</button>
         </div>
     );
