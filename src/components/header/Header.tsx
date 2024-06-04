6667462bdd7218dcfa0c959a6c93bf169c45e230
nm090909 <nm09090@naver.com>
2024-06-05 01:32:17 +0900

    [design update]

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,6 +4,7 @@ export default function Header() {
     return (
         <div className="header">
             <a className="header_logo_block">
+                <div className={"logo-img"}/>
                 <h2>KWS (KLE445 서버대여시스템)</h2>
             </a>
         </div>
